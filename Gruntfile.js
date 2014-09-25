864e0eae21612273f46a9d5f98ec070e0a216532
yuhao(羽昊) <yuhao@taobao.com>
2014-09-25 10:25:54 +0800

    修复copy images task

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: '.',
-                    src: ['res'],
+                    src: ['res/**/*'],
                     dest: 'build'
                 }]
             },
