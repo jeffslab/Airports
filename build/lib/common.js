3b3b2a94258bdd7293f59caf5ec15c09548f19d8
yuhao(羽昊) <yuhao@taobao.com>
2014-10-16 14:32:03 +0800

    add build

--- build/lib/common.js
+++ build/lib/common.js
@@ -1,2 +1,2 @@
-/*! AirPorts 15-10-2014 */
+/*! AirPorts 16-10-2014 */
 (function(){var t=function(){var t,e=document.getElementsByTagName("html")[0];return function(){var n=Math.min(e.offsetWidth,480);t!=n&&(t=n,e.style.fontSize=10/480*t+"px")}}();t(),window.addEventListener("resize",function(){setTimeout(t,200)},!1)})();
